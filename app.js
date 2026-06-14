0e039a19a79e2fcbab5ed3a1e169ac8b26ba6fe1
parv gajipara <parvgajipara2508@gmail.com>
2026-06-14 11:31:49 +0530

    complete reviews and validation

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const path = require("path")
 const methodOverride = require("method-override")
 const ejs = require('ejs');
 const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
+const { listingSchema, reviewSchema } = require("./schema.js")
+const Reviews = require("./models/review.js")
+
+const listings = require("./routes/listing.js")
+
+
+
 
 app.engine("ejs", ejsMate)
 app.set("view engine", "ejs")
@@ -35,53 +44,63 @@ app.get("/", (req, res) => {
     res.send("working")
 })
 
-//index route
-app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({})
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body)
 
-    res.render("listings/index.ejs", { allListings })
-})
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, errMsg);
+    } else {
+        next()
+    }
+}
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body)
 
-//new route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs")
-})
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, errMsg);
+    } else {
+        next()
+    }
+}
 
-//show route
-app.get("/listings/:id", async (req, res) => {
-    let { id } = req.params
-    const listing = await Listing.findById(id)
-    res.render("listings/ahow.ejs", { listing })
-})
+app.use("/listings", listings)
 
-//create route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing)
-    await newListing.save()
-    res.redirect("/listings")
 
-})
+//reviews
+//post route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Reviews(req.body.review)
 
-//edit route
-app.get("/listings/:id/edit", async (req, res) => {
-    let { id } = req.params
-    const listing = await Listing.findById(id)
-    res.render("listings/edit.ejs", { listing })
-})
+    listing.reviews.push(newReview);
 
-//update route
-app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing })
-    res.redirect(`/listings/${id}`)
-})
+    await newReview.save();
+    await listing.save();
 
+
+    res.redirect(`/listings/${listing._id}`);
+}))
+
+//reviews
 //delete route
-app.delete("/listings/:id", async (req, res) => {
-    let { id } = req.params
-    let deleteListing = await Listing.findByIdAndDelete(id)
-    res.redirect("/listings")
-    res.redirect
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Reviews.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`)
+}))
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
+})
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Someting went wrong!" } = err;
+    res.status(statusCode).render("error.ejs", { message })
+    // res.status(statusCode).send(message);
 })
 
 
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const Review = require("./review.js")
 
 const listingSchema = Schema({
     title: {
@@ -17,11 +18,24 @@ const listingSchema = Schema({
             type: String,
         },
     },
-    price: String,
+    price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { in: listing.reviews } })
+    }
+
+})
 
 const Listing = mongoose.model("Listing", listingSchema)
 module.exports = Listing;
